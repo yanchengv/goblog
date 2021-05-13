6fb9be9ad6328429743d0365ca2f15d339f46107
yanchengv <zqalyc@163.com>
2021-05-13 16:15:38 +0800

    校验登录转发

--- controllers/crms/sessions.go
+++ controllers/crms/sessions.go
@@ -7,12 +7,12 @@ import (
 )
 
 func AuthUser() gin.HandlerFunc {
-	return func(c *gin.Context){
-		userId,err := c.Cookie("userID")
+	return func(c *gin.Context) {
+		userId, err := c.Cookie("userID")
 		if err != nil || userId == "" {
 			//获取cookie报错或者userId为空则跳转登录界面
 			fmt.Println("请先登录")
-			c.Redirect(http.StatusMovedPermanently, "/crms/login")
+			c.Redirect(http.StatusTemporaryRedirect, "/crms/login")
 		}
 		c.Next()
 	}
