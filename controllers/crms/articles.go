f63322e6e96f98ec59074ad42632c0e38ebdca53
yanchengv <zqalyc@163.com>
2021-05-21 17:01:38 +0800

    7  master -> master

--- controllers/crms/articles.go
+++ controllers/crms/articles.go
@@ -12,16 +12,16 @@ import (
 func ArticleIndex(c *gin.Context) {
 	//header的面包屑菜单
 	breadcrumb := map[string]string{"m1": "数据管理", "m2": "文章管理", "m2url": "#", "m3": "文章列表"}
-	pageSize := 2
 	var articles []models.Article
 	var totalCount int64
+	pageSize := 20
 	page, _ := strconv.Atoi(c.DefaultQuery("page", "0"))
 
 	//创建一个分页器，100条数据，每页10条
 	models.DB.Model(&models.Article{}).Count(&totalCount)
 	pagination := pagination2.Initialize(c.Request, totalCount, pageSize)
 	//分页查询记录
-	models.DB.Scopes(models.Paginate(page, 15)).Find(&articles)
+	models.DB.Scopes(models.Paginate(page, pageSize)).Find(&articles)
 	c.HTML(http.StatusOK, "articles/index.html", gin.H{
 		"title":      "文章列表",
 		"breadcrumb": breadcrumb,
